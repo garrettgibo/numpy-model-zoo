8ad12837fae57b710be8e3b669207cae92d2f0e0
Garrett Gibo <garrett.gibo@gmail.com>
2020-06-06 15:42:56 -0700

    add linear regression

--- main.py
+++ main.py
@@ -14,24 +14,30 @@ import click
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from modelzoo.models import InvalidModel, DecisionTree, RandomForest
+from modelzoo.models import (
+    InvalidModel,
+    DecisionTree,
+    RandomForest,
+    LinearRegression,
+)
 
 
 MODELS = {
     "decision-tree": DecisionTree,
     "random-forest": RandomForest,
+    "linear-regression": LinearRegression
 }
 MODELS_HELP = ','.join(list(MODELS.keys()))
 
 
-def load_data():
+def load_data(test_size):
     url_Wine = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
     wine = pd.read_csv(url_Wine, delimiter=';')
     X = np.array(wine)[:, :-1]
     y = np.array(wine)[:, -1]
     y = np.array(y.flatten()).astype(int)
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=0
+        X, y, test_size=test_size, random_state=0
     )
     return X_train, X_test, y_train, y_test
 
@@ -39,16 +45,18 @@ def load_data():
 @click.command()
 @click.option("-m", "--model-type", default=None, help=MODELS_HELP)
 def main(model_type):
-    X_train, X_test, y_train, y_test = load_data()
+    # load data and do a train test split
+    X_train, X_test, y_train, y_test = load_data(test_size=0.2)
 
-    # Load, initialize, and fit model
     model = MODELS.get(model_type, lambda: InvalidModel())
-    clf = model()
+    clf = model()  # initialize model with default parameters
     clf.fit(X_train, y_train)
 
     print("-"*10, clf, "-"*10)
-    clf.score(X_train, y_train, datatype="Train")
-    clf.score(X_test, y_test)
+    score_train = clf.score(X_train, y_train)
+    score_test = clf.score(X_test, y_test)
+    print(f"{clf} - Train {clf.metric} -> {score_train}")
+    print(f"{clf} - Test {clf.metric} -> {score_test}")
 
 
 if __name__ == "__main__":
